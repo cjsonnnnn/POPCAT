90571c57e83947ea9aff081fd2fe0bfc14fc46ed
jsonnnnn <cjsonnnnn@gmail.com>
2024-01-10 23:48:23 +0700

    add parallel execution with multiprocessing

--- popcat-automated.py
+++ popcat-automated.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import concurrent.futures
 import time
 
 
@@ -14,9 +15,10 @@ def gt(startTime, endTime, curTotal):
     return curTotal
 
 
-def run(numClicks: int, numIteration: int):
+def main(pId, numClick: int, numIteration: int=1000):
+    print(f"Process {pId} started with numClicks={numClick} and numIteration: {numIteration}")
     driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager("115.0.5790.102").install())
+        service=ChromeService(ChromeDriverManager().install())
     )
     driver.get("https://popcat.click/")
     driver.maximize_window()
@@ -25,13 +27,13 @@ def run(numClicks: int, numIteration: int):
     element = wd.until(EC.element_to_be_clickable((By.XPATH, "//body/div[1]")))
     k = 100 / numIteration
     l = ((100 // k) - 1) if ((100 / k).is_integer()) else (100 // k)
-    devider = int(numClicks * (k / 100))
+    devider = int(numClick * (k / 100))
     total_elapsed_time = 0
     j = 0
     start_time = time.time()
-    for i in range(1, numClicks + 1):
+    for i in range(1, numClick + 1):
         element.click()
-        if (i == numClicks) and (j >= l):
+        if (i == numClick) and (j >= l):
             end_time = time.time()
             print(f"\nat 100%: {i} clicks finished")
             total_elapsed_time = gt(start_time, end_time, total_elapsed_time)
@@ -39,13 +41,22 @@ def run(numClicks: int, numIteration: int):
 
         if (i % devider == 0) and (j < l):
             end_time = time.time()
-            print(f"\nat {round((i/numClicks)*100, 2)}%: {i} clicks finished")
+            print(f"\nat {round((i/numClick)*100, 2)}%: {i} clicks finished")
             total_elapsed_time = gt(start_time, end_time, total_elapsed_time)
             start_time = time.time()
             j += 1
-
+    print(f"Process {pId} completed")
 
 if __name__ == "__main__":
-    numClicks = int(input("Enter number of clicks: "))
-    numIteration = int(input("Enter number of output iteration: "))
-    run(numClicks, numIteration)
+    numWorkers = int(input("Number of workers: "))
+    numClicks = numClicks if len(numClicks := str(input("Number of click each worker (e.g. 30 20 100): ")).split(" ")) == numWorkers else 0
+    if numClicks:
+        numClicks = [int(num) for num in numClicks]
+    else:
+        raise ValueError
+    numIteration = 1000
+    numIteration = numIteration if (i:=input(f"Enter number of output iteration for all the workers [{numIteration}]: ")) == '' else int(i)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=numWorkers) as pool:
+        pIds = list(range(numWorkers))
+        pool.map(main, *[pIds, numClicks, [numIteration]*numWorkers])
